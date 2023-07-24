ab943fc40076b9ca7ba39838fbae2909bf158498
Elena <ela1-1-1@mail.ru>
2023-07-24 17:53:06 +0500

    Дз_семинар4

---
+++ HW4/Program.cs
@@ -0,0 +1,46 @@
+﻿/*Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+3, 5 -> 243 (3⁵)
+2, 4 -> 16*/
+
+/*Console.Write("Введите число a: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число b: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int count = 1;
+int result = a;
+while (count < b)
+{
+    result = result * a;
+    count++;
+}
+Console.WriteLine(result);*/
+
+/*Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+452 -> 11
+82 -> 10
+9012 -> 12*/
+
+/*Console.Write("Введите число: ");
+int num = Convert.ToInt32(Console.ReadLine());
+int sum = 0;
+while (num > 0)
+{
+    sum = sum + num % 10;
+    num = num / 10;
+}
+Console.WriteLine(sum);*/
+
+
+/*Задача 29: Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+6, 1, 33 -> [6, 1, 33]*/
+
+/*int [] array = new int [8];
+Console.Write("[");
+for (int i = 0; i < array.Length; i++)
+{
+    array[i] = new Random().Next(1,101);
+    Console.Write($"{array[i]}, ");
+}
+Console.Write("\b\b]");*/
